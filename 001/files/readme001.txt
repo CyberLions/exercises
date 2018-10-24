03d1dd1743b00435cfcac6ced0bc850acf65a155
Unknown <pqh5113@gmail.com>
2018-10-24 18:52:58 -0400

    improvements to 001

--- 001/files/readme001.txt
+++ 001/files/readme001.txt
@@ -1,1 +1,2 @@
 TASK: Find how many times people visited /shuttle/missions/sts-71/mission-sts-71.html
+Use access.log to complete this task!
--- 001/src.sh
+++ 001/src.sh
@@ -18,19 +18,20 @@ sudo chmod +x /etc/update-motd.d/00-start
 sudo run-parts /etc/update-motd.d/
 
 # get log file and readme with challenge instructions
-sudo curl https://raw.githubusercontent.com/CyberLions/ccso-exercises/exercises/001/files/access.log > ~/access.log
-sudo curl https://raw.githubusercontent.com/CyberLions/ccso-exercises/exercises/001/files/readme001.txt > ~/readme001.txt
+sudo curl -o /home/ccso001/access.log https://raw.githubusercontent.com/CyberLions/ccso-exercises/exercises/001/files/access.log
+sudo curl -o /home/ccso001/readme001.txt https://raw.githubusercontent.com/CyberLions/ccso-exercises/exercises/001/files/readme001.txt
 
 # only ccso001 user can access
-sudo chown ccso001 ~/access.log
-sudo chgrp ccso001 ~/access.log
-sudo chmod 0660 ~/access.log
-sudo chown ccso001 ~/readme001.txt
-sudo chgrp ccso001 ~/readme001.txt
-sudo chmod 0660 ~/readme001.txt
+sudo chown ccso001 /home/ccso001/access.log
+sudo chgrp ccso001 /home/ccso001/access.log
+sudo chmod 0660 /home/ccso001/access.log
+sudo chown ccso001 /home/ccso001/readme001.txt
+sudo chgrp ccso001 /home/ccso001/readme001.txt
+sudo chmod 0660 /home/ccso001/readme001.txt
 
 echo "==========================================================="
 echo "Setup completed!"
 echo "To start, type: su ccso001, with password: Y2NzbyBjaGFsbGVuZ2UgMDAx"
+echo "    cd ~"
 echo "and check out the readme001.txt file"
 echo "==========================================================="
