0dd5a2565cc7cb7234f23bfd2b47e8a1f862c929
Unknown <pqh5113@gmail.com>
2018-10-25 18:51:56 -0400

    add exercise 002

---
+++ 002/files/readme002.txt
@@ -0,0 +1,12 @@
+==============================
+
+Dear friend,
+
+ Help! A hacker did something
+  to my precious family photo!
+  Please figure out what they
+  did, and get my photo back!
+
+ - Jerry
+
+==============================
---
+++ 002/src.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# update and install dependencies
+sudo apt-get -qq update #; sudo apt-get -y upgrade
+sudo apt-get -qq -y install curl nmap
+
+# create users with home directory and bash as shell
+sudo /usr/sbin/useradd -m -s /bin/bash ccso002
+
+# set passwords
+sudo echo ccso002:d2VsY29tZSB0byBsZXZlbCAy | sudo chpasswd
+
+# get log file and readme with challenge instructions
+sudo curl -o /home/ccso002/readme002.txt https://raw.githubusercontent.com/CyberLions/ccso-exercises/exercises/002/files/readme002.txt
+sudo curl -o /home/ccso002/readme002.txt https://raw.githubusercontent.com/CyberLions/ccso-exercises/exercises/002/files/DSC_0043.JPG
+
+# only ccso002 user can access
+sudo chown ccso002 /home/ccso002/*
+sudo chgrp ccso002 /home/ccso002/*
+sudo chmod 0660 /home/ccso002/*
+
+echo "==========================================================="
+echo "Setup completed!"
+echo "To start, type: su ccso002, with password: d2VsY29tZSB0byBsZXZlbCAy"
+echo "    cd ~"
+echo "and check out the readme002.txt file"
+echo "==========================================================="
